0a938f157f10106f851fe6633d5ecce0c897ab71
sonakshibhalla <bhallasonakshi@gmail.com>
2021-08-27 14:10:38 -0700

    Revert "hi"
    This reverts commit 4a66838b82de601776426c7641ef4305fe483f7c.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 # create a Flask instance
 app = Flask(__name__)
@@ -26,15 +26,10 @@ def walruses():
 def hawkers():
     return render_template("hawkers.html")
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
-    # starting and empty input default
-    return render_template("greet.html", name="World")
+
+@app.route('/stub/')
+def stub():
+    return render_template("stub.html")
 
 
 # runs the application on the development server
