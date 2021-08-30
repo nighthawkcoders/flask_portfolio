b0f6fc39f4c38171cd142cf835017e2c670c3be7
Reinhardtlotter <89236857+Reinhardtlotter@users.noreply.github.com>
2021-08-30 12:49:00 -0700

    commit test

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ def hawkers():
 def stub():
     return render_template("stub.html")
 
+@app.route('/greet/')
+def greet():
+    return render_template("greet.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
