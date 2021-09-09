9b1596f656083ca3995ef2054abbaf451ddfec19
Brian Zhang <brianzhang2016@gmail.com>
2021-09-09 13:08:33 -0700

    k

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -31,6 +31,15 @@ def hawkers():
 def stub():
     return render_template("stub.html")
 
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", name=name)
+    # starting and empty input default
+    return render_template("greet.html", name="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
