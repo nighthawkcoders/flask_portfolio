8fa6ee556584581a8a1e21059f9ae2652dea5a44
Saumya Palk <saumyp3@gmail.com>
2021-09-13 12:20:26 -0700

    Greet Dropdown

--- main.py
+++ main.py
@@ -60,6 +60,10 @@ def kashish():
 def concepts():
     return render_template("concepts.html")
 
+@app.route('/greet/')
+def greet():
+    return render_template("greet.html")
+
 
 # runs the application on the development server
 if __name__ == "__main__":
