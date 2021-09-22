f7743ca91cf8440a36db54824dd58c6036f9021d
kashishchopra <kc9puppies@gmail.com>
2021-09-22 13:11:00 -0700

    added rgb page for kashish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
-from algorithms.image import sonakshi_image_data
-from algorithms.image import kashish_image_data
+from algorithms.image import sonakshi_image_data, kashish_image_data
 
 
 # create a Flask instance
