198c3cd018e7f8d134aacabc3815b1da4726d6d4
kashishchopra <kc9puppies@gmail.com>
2021-09-22 12:55:51 -0700

    added kashishrgb.html

--- algorithms/image.py
+++ algorithms/image.py
@@ -62,4 +62,47 @@ def sonakshi_image_data(path="static/assets/sonakshiimages/", img_list=None):
         img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
     return img_list  # list is returned with all the attributes for each image dictionary
 
-
+def kashish_image_data(path="static/assets/kashishimages/", img_list=None):
+    if img_list is None:  # color_dict is defined with defaults
+        img_list = [
+            {'source': "Apples", 'label': "Red", 'file': "red.png"},
+            {'source': "Blue", 'label': "Blue", 'file': "blue.png"},
+            {'source': "Green", 'label': "Green", 'file': "green.png"},
+            {'source': "Orange", 'label': "Orange", 'file': "orange.png"},
+        ]
+    # gather analysis data and meta data for each image, adding attributes to each row in table
+    for img_dict in img_list:
+        img_dict['path'] = '/' + path  # path for HTML access (frontend)
+        file = path + img_dict['file']  # file with path for local access (backend)
+        # Python Image Library operations
+        img_reference = Image.open(file)  # PIL
+        img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/
+        img_dict['format'] = img_reference.format
+        img_dict['mode'] = img_reference.mode
+        img_dict['size'] = img_reference.size
+        # Conversion of original Image to Base64, a string format that serves HTML nicely
+        img_dict['base64'] = image_formatter(img_reference, img_dict['format'])
+        # Numpy is used to allow easy access to data of image, python list
+        img_dict['data'] = numpy.array(img_data)
+        img_dict['hex_array'] = []
+        img_dict['binary_array'] = []
+        # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
+        for pixel in img_dict['data']:
+            # hexadecimal conversions
+            hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
+            hex_value = hex_value.replace("x", "0")
+            img_dict['hex_array'].append("#" + hex_value)
+            # binary conversions
+            bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
+            img_dict['binary_array'].append(bin_value)
+        # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
+        img_dict['gray_data'] = []
+        for pixel in img_dict['data']:
+            average = (pixel[0] + pixel[1] + pixel[2]) // 3
+            if len(pixel) > 3:
+                img_dict['gray_data'].append((average, average, average, pixel[3]))
+            else:
+                img_dict['gray_data'].append((average, average, average))
+        img_reference.putdata(img_dict['gray_data'])
+        img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
+    return img_list  # list is returned with all the attributes for each image dictionary
