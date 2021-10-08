ee70129b6633a167cac8225b7bf8e4b53c0aef38
Reinhardtlotter <89236857+Reinhardtlotter@users.noreply.github.com>
2021-10-08 11:35:44 -0700

    Finished Color Code

---
+++ algorithms/colorcode.py
@@ -0,0 +1,9 @@
+student_list = ['pam','rob','joe','greg','bob','amy','matt']
+print(student_list[2:5])
+print(student_list[:-5])
+print(student_list[5:])
+print(student_list)
+if 'rob' in student_list:
+    print('Rob is in Student List')
+else:
+    print('Rob is not in Student list')
