2547563ee5aea1bf53cc791430e87fcc50c5cee0
ProRichyMan <prorichyman@gmail.com>
2021-10-08 11:24:54 -0700

    Start for Color Codes

--- templates/list.py
+++ templates/list.py
@@ -40,5 +40,51 @@ average = Average(lst)
 # Printing average of the list
 print("Average of the list =", round(average, 2))
 
+# TT7
+student_list = ['pam','rob','joe','greg','bob','amy','matt']
+# Activity 1
+print(student_list[2:5])
+print(student_list[:-5])
+print(student_list[5:])
+print(student_list)
+if 'rob' in student_list:
+    print("Rob is in student_list")
+else:
+    print("Rob is not in student_list")
+
+# Activity 2
+p1 = { "name":"John", "age":61, "city":"Eugene"}
+p2 = { "name":"Risa", "age":16, "city":"New York"}
+p3 = { "name":"Ryan", "age":16, "city":"Los Angeles"}
+p4 = { "name":"Shekar", "age":16, "city":"San Francisco"}
+# a list of dictionaries
+list_of_people = [p1, p2, p3, p4]
+
+
+# turn list to dictionary of people
+dict_people = {'people': list_of_people}
+print("List to Dictionary of people")
+print(type(dict_people))
+print(dict_people)
+# write some code to Print People from Dictionary
+# turn dictionary to JSON
+print("** Dumps - Python to JSON String**")
+json_people = json.dumps(list_of_people)
+print("JSON People #1")
+print(type(json_people))
+print(json_people)
+# write some code to pretty print the JSON dict
+
+
+# write some code to unwind JSON using json.loads and print the people
+print("** Loads - JSON to Python Dict**")
+#json_dict = json.loads(json_people)
+print(json_dict)
+# to list
+names = [person['name'] for person in json_dict]
+print("Names of people to list: " + str(names))
+print("Names of people: ")
+# pretty print Names of People
+
 if __name__ == "__main__":
     app.run(debug=True)
