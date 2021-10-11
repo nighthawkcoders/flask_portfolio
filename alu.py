3e0407d2dc4c0a2d18fdd38c630b6ba9af9517e9
DistilledVinegar <ericw5845@outlook.com>
2021-10-10 17:20:22 -0700

    ALU fully functional (as a Python script lolol, not integrated into webpage yet oc)

--- alu.py
+++ alu.py
@@ -25,13 +25,17 @@ def xor(a, b):
 	else:
 		return False
 
+def Reverse(lst):
+	new_lst = lst[::-1]
+	return new_lst
+
 carry = False
 def oneBit(a, b, c=False):
-	sum = xor(xor(a, b), c)
+	holesum = xor(xor(a, b), c)
 	global carry
 	carry = orGate(andGate(a, b), andGate(c, xor(a,b)))
 	#See this: https://youtu.be/QZwneRb-zqA?t=639
-	return (sum, carry)
+	return (holesum, carry)
 
 def decToBin(dec):
 	dec = int(dec)
@@ -39,53 +43,70 @@ def decToBin(dec):
 	while dec >= 2**bitlen:
 		bitlen += 1 #determine bitlength in binary
 
-	byte1=[] #create an empty bus
+	bus=[] #create an empty bus
 
 	while bitlen > 0: #do this for every bit
 		if dec - 2**(bitlen-1) >= 0:
 			dec = dec - 2**(bitlen-1)
-			byte1.append(True)
+			bus.append(True)
 			bitlen -= 1
-			print(dec)
 		else:
-			byte1.append(False)
+			bus.append(False)
 			bitlen -= 1
-			print(dec)
-
-	return byte1
+	return bus
 
 print("Enter your first number in base-10.")
-num1 = input()
-byte1 = decToBin(num1)
-print(byte1)
+input1 = input()
+byte1 = ""
+for i in decToBin(input1):
+	if i == True:
+		byte1 = byte1 + "1"
+	else:
+		byte1 = byte1 + "0"
+print(input1 + " is " + byte1 + " in binary.")
 
 print("Enter your second number in base-10.")
-num2 = input()
-byte2 = decToBin(num2)
-print(byte2)
-
-
-#might need to reverse bit order in each byte?
-#ok lol maybe make a global carry var like last time
-
-while len(byte2) != bitness:
-	b = input()
-	if b == "1":
-		byte2.append(True)
-	elif b == "0":
-		byte2.append(False)
+input2 = input()
+byte2 = ""
+for i in decToBin(input2):
+	if i == True:
+		byte2 = byte2 + "1"
 	else:
-		print("Only valid inputs are 0 and 1.")
+		byte2 = byte2 + "0"
+print(input2 + " is " + byte2 + " in binary.")
 
+num1 = [False] + decToBin(input1)
+num2 = [False] + decToBin(input2)
 
-for count, b in enumerate(byte1):
-	n = -abs(count + 1)  #Determine which BIT we're working with
-	byte3.append(oneBit(byte1[n], byte2[n], carry)) #Append to byte 3 the result of a One-bit operation
+while len(num1) > len(num2):
+	num2 = [False] + num2
+while len(num2) > len(num1):
+	num1 = [False] + num1
 
-print("Byte 3:")
+num3 = []
+byte3 = ""
 
-for i, j in enumerate(byte3):
-	if byte3[i][0] == True:
-		print("1")
+num1 = Reverse(num1)
+num2 = Reverse(num2)
+
+for count, b in enumerate(num1):
+	num3.append(oneBit(num1[count], num2[count], carry))
+
+for count, i in enumerate(num3):
+	if num3[count][0] == True:
+		byte3 = "1" + byte3
 	else:
-		print("0")
+		byte3 = "0" + byte3
+
+#bin to dec
+output = 0
+bitpos = 0
+for i in num3:
+	if i[0] == True:
+		output += 2**bitpos
+		bitpos += 1
+	else:
+		bitpos += 1
+
+print("These add up to " + str(byte3) + " in binary.")
+print("The decimal output is " + str(output)+".")
