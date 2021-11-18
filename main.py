88eaddb9d173a2a89d5f2501414eb163d35f2702
tanayrayavarapu <tanayrayavarapu@gmail.com>
2021-11-17 22:52:13 -0800

    Toggle Page Updated (Need to Add Images, having problems)

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def index():
 def connor():
     return render_template('connor.html')
 
+@app.route('/tanay/')
+def tanay():
+    return render_template('tanay.html')
 
 # runs the application on the development server
 if __name__ == "__main__":
