ad81330ff91bbdebf9c9e35ad64c5c7e3b42a4f8
connorw72 <connorwang2@gmail.com>
2021-11-17 22:25:15 -0800

    image toggle

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # import "packages" from flask
 from flask import Flask, render_template
-from algorithms.connor import c_image
 from pathlib import Path
 
 # create a Flask instance
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/connor/')
 def connor():
-    path = Path(app.root_path) / "static" / "assets"
-    return render_template('connor.html', images=c_image(path))
+    return render_template('connor.html')
 
 
 # runs the application on the development server
