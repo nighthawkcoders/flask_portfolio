a012e1c8c5c09660fe152c6bc0b111b9d5dd184e
Saathvika Ajith <saathvika.ajith@gmail.com>
2021-11-30 13:49:22 -0800

    individual homepages

--- main.py
+++ main.py
@@ -11,22 +11,6 @@ def index():
     return render_template("index.html")
 
 
-# connects /kangaroos path to render kangaroos.html
-@app.route('/kangaroos/')
-def kangaroos():
-    return render_template("kangaroos.html")
-
-
-@app.route('/walruses/')
-def walruses():
-    return render_template("walruses.html")
-
-
-@app.route('/hawkers/')
-def hawkers():
-    return render_template("hawkers.html")
-
-
 @app.route('/samaya/')
 def samaya():
     return render_template("samaya.html")
