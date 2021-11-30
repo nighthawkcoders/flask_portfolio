acfae179881f8d151f5522f9181ff753b57a84e0
khush <khushibagri24@gmail.com>
2021-11-30 15:21:34 -0800

    khushi html picture

--- main.py
+++ main.py
@@ -61,9 +61,31 @@ def genius():
 def linda():
     return render_template("linda.html")
 
-@app.route('/khushi')
+@app.route('/khushi', methods=['GET', 'POST'])
 def khushi():
-    return render_template("khushi.html")
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("khushi.html", name=name)
+    # starting and empty input default
+    return render_template("khushi.html", name="World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/newapi', methods=['GET', 'POST'])
 def newapi():
