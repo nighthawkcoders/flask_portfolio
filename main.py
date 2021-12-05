205086624720fb482ebe216f0412457a5f1df8f3
SanjayB06 <sanjaymbharadwaj@gmail.com>
2021-12-05 14:13:55 -0800

    dark theme skeleton

--- main.py
+++ main.py
@@ -43,7 +43,19 @@ def sanjay():
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     output =json.loads(response.text)
-    return render_template("about_us/sanjay.html",data=output)
+    url2 = "https://geek-jokes.p.rapidapi.com/api"
+
+    querystring2 = {"format":"json"}
+
+    headers2 = {
+        'x-rapidapi-host': "geek-jokes.p.rapidapi.com",
+        'x-rapidapi-key': "6279ac9b7amsh7dc015c7d7746fbp1f4d65jsn125b0c500438"
+    }
+
+    response2 = requests.request("GET", url2, headers=headers2, params=querystring2)
+
+    output2 = json.loads(response2.text)
+    return render_template("about_us/sanjay.html",data=output,joke=output2)
 # runs the application on the development server
 @app.route('/flashcards/')
 def flashcards():
