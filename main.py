438255466a591d46e8f7bf92419f7a1797601ddf
ProRichyMan <prorichyman@gmail.com>
2021-12-08 13:16:56 -0800

    Start on API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
+from nathan.nathan import nathan_bp
 # create a Flask instance
 app = Flask(__name__)
 
@@ -37,7 +39,9 @@ def aboutdaniel():
 def mainabout():
     return render_template("mainabout.html")
 
+app.register_blueprint(nathan_bp)
 
+app.run(host="127.0.0.1", port=5000)
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
