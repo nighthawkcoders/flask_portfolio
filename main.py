84211f8ab03a1b89266ded0046e4da35cc98b2b5
mfcbpranav <mfcb.pranav@gmail.com>
2021-12-09 13:54:38 -0800

    Pranav API Images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import "packages" from flask
 from flask import Flask, render_template
+import requests
 from pathlib import Path
 
 # create a Flask instance
@@ -27,7 +28,18 @@ def colin():
 
 @app.route('/pranav/')
 def pranav():
-    return render_template('pranav.html')
+    url = "https://free-football-soccer-videos.p.rapidapi.com/"
+
+    headers = {
+        'x-rapidapi-host': "free-football-soccer-videos.p.rapidapi.com",
+        'x-rapidapi-key': "a52c00a742mshc6c13b49056f1c7p11b54cjsnc5e7c51a6001"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    data = response.json()
+
+    print(response.json())
+    return render_template('pranav.html', data=data[-5:])
 
 
 
