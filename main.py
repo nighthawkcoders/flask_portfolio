122dbebc4a83806726d0c5602b01fffe4695d55d
alice <tang.alice.000@gmail.com>
2021-12-10 01:00:42 -0800

    alice about me page

--- main.py
+++ main.py
@@ -21,17 +21,7 @@ def samaya():
 
 @app.route('/alice/')
 def alice():
-    url = "https://brianiswu-cat-facts-v1.p.rapidapi.com/facts"
-
-    headers = {
-        'x-rapidapi-host': "brianiswu-cat-facts-v1.p.rapidapi.com",
-        'x-rapidapi-key': "f877084053msh82cfa972b631ab7p1c4893jsn442e2f514bb0"
-    }
-
-    response = requests.request("GET", url, headers=headers)
-    # how long is the response.json, 0-4, random, 0-8
-    output = response.json()     #5 items long
-    return render_template("alice.html", output=output)
+    return render_template("alice.html")
 
 
 @app.route('/pranavi/')
