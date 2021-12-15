3a3d9821d29616d78e57af30a6c44881e7e87c70
Danny4w <89228041+Danny4w@users.noreply.github.com>
2021-12-15 12:12:04 -0800

    worked on language translator

--- main.py
+++ main.py
@@ -49,6 +49,15 @@ def feedback():
             return render_template("layouts/feedback.html", input=input, name=name)
     return render_template("layouts/feedback.html")
 
+@app.route('/translator/', methods=['GET', 'POST'])
+def translator():
+    if request.form:
+        input = request.form.get("feed1")
+        name = request.form.get("feed2")
+        if len(input) != 0:  # input field has content
+            return render_template("layouts/translator.html", input=input, name=name)
+    return render_template("layouts/translator.html")
+
 
 # runs the application on the development server
 # changed port from 5000 to 8080 to test gunicorn
