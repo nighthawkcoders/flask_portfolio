6695307c29aeae30aab9ad9e6ded58d2db14b25e
Kolin <colin.howard415@icloud.com>
2021-12-15 20:47:58 -0800

    Crud Project Proper

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from crud import app_crud
+from __init__ import app
+from flask import Blueprint
+import requests
+import ssl
+
 
 from blueprints.connor import bconnor
 from blueprints.pranav import bpranav
@@ -7,7 +13,7 @@ from blueprints.colin import bcolin
 from blueprints.chase import bchase
 from blueprints.tanay import btanay
 # create a Flask instance
-app = Flask(__name__)
+app.register_blueprint(app_crud)
 app.register_blueprint(bconnor, url_prefix="/")
 app.register_blueprint(bpranav, url_prefix="/")
 app.register_blueprint(bcolin, url_prefix="/")
