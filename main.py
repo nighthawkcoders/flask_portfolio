1aa85e7f449f1566be980b565e542f40640722ea
Gavin Wu <wugavin0573@gmail.com>
2021-12-17 00:26:05 -0800

    Gavin - 404 Page

--- main.py
+++ main.py
@@ -83,6 +83,11 @@ def sports_cards():
 def funko_pops():
     return render_template("funko_pops.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 
 # runs the application on the development server
 if __name__ == "__main__":
