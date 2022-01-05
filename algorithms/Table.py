41d9c94713dd92fedda1189a510137e88bced972
chase <Chaseott123@gmail.com>
2022-01-04 21:27:06 -0800

    Habistat

---
+++ algorithms/Table.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+
+table = [['First Name', 'Last Name', 'Age'], ['John', 'Smith', 39], ['Mary', 'Jane', 25], ['Jennifer', 'Doe', 28]]
+
+print(tabulate(table, tablefmt='html'))
+
+
+def sussy():
+    return table
--- blueprints/chase.py
+++ blueprints/chase.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, render_template
 import requests
 from algorithms.ChaseAPI import game
+from algorithms.Table import sussy
+
 bchase = Blueprint("bchase", __name__, static_folder="static", template_folder="templates")
+
 
 @bchase.route('/Chase/')
 def chase():
     return render_template('Chase.html', fgame=game())
 
+
 @bchase.route('/cats/')
 def cats():
     return render_template('cat.html')
+
+
+@bchase.route('/Habitats/')
+def habitats():
+    return render_template('Habitats.html', sussy=sussy())
+
