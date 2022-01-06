07f6e7f7fb9acc999d24e81333b8e96084bca8a0
tanayrayavarapu <tanayrayavarapu@gmail.com>
2022-01-06 14:21:15 -0800

    PBL Features Unfinished

--- blueprints/pranav.py
+++ blueprints/pranav.py
@@ -5,18 +5,7 @@ bpranav = Blueprint("bpranav", __name__, static_folder="static", template_folder
 
 @bpranav.route('/pranav/')
 def pranav():
-    url = "https://free-football-soccer-videos.p.rapidapi.com/"
-
-    headers = {
-        'x-rapidapi-host': "free-football-soccer-videos.p.rapidapi.com",
-        'x-rapidapi-key': "a52c00a742mshc6c13b49056f1c7p11b54cjsnc5e7c51a6001"
-    }
-
-    response = requests.request("GET", url, headers=headers)
-    data = response.json()
-
-    print(response.json())
-    return render_template('pranav.html', data=data[-5:])
+    return render_template('pranav.html')
 
 @bpranav.route('/dogs/')
 def dogs():
