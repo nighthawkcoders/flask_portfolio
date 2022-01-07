e3300a57c423bf6d1f6dcd57727b9c3e164e76c9
tanayrayavarapu <tanayrayavarapu@gmail.com>
2022-01-07 15:11:33 -0800

    Background Colors and Layouts Updated

--- blueprints/colin.py
+++ blueprints/colin.py
@@ -5,21 +5,7 @@ bcolin = Blueprint("bcolin", __name__, static_folder="static", template_folder="
 
 @bcolin.route('/colin/')
 def colin():
-    url = "https://free-nba.p.rapidapi.com/players"
-    return render_template('colin.html', data=data)
-
-querystring = {"page":"0","per_page":"25"}
-
-headers = {
-    'x-rapidapi-host': "free-nba.p.rapidapi.com",
-    'x-rapidapi-key': "c9627c0b20msh49d43de4e2620d8p16c0f2jsn67c02d827b55"
-    }
-
-response = requests.request("GET", url = "https://free-nba.p.rapidapi.com/players", headers=headers, params=querystring)
-data = response.json()['data']
-
-print(data)
-
+    return render_template('colin.html')
 
 @bcolin.route('/imageCarousel/')
 def imageCarousel():
