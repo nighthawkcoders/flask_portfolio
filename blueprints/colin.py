d22f3c9bc1fd1a5acd8067fa8fca75ec25b2a1a3
connorw72 <connorwang2@gmail.com>
2022-01-07 15:08:43 -0800

    db

--- blueprints/colin.py
+++ blueprints/colin.py
@@ -5,7 +5,20 @@ bcolin = Blueprint("bcolin", __name__, static_folder="static", template_folder="
 
 @bcolin.route('/colin/')
 def colin():
-    return render_template('colin.html')
+    url = "https://free-nba.p.rapidapi.com/players"
+    return render_template('colin.html', data=data)
+
+querystring = {"page":"0","per_page":"25"}
+
+headers = {
+    'x-rapidapi-host': "free-nba.p.rapidapi.com",
+    'x-rapidapi-key': "c9627c0b20msh49d43de4e2620d8p16c0f2jsn67c02d827b55"
+}
+
+response = requests.request("GET", url = "https://free-nba.p.rapidapi.com/players", headers=headers, params=querystring)
+data = response.json()['data']
+
+print(data)
 
 
 @bcolin.route('/imageCarousel/')
