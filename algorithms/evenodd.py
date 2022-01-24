f66cf04a1ba453c4df8bee4babd6ca927abb33d5
connorw72 <connorwang2@gmail.com>
2022-01-24 14:15:54 -0800

    evenodd

--- algorithms/evenodd.py
+++ algorithms/evenodd.py
@@ -3,4 +3,13 @@ if(num1%2==0):
     print("{} is even".format(num1))
 else:
     print("{} is odd".format(num1))
-elif:
+
+if (num1>=1):
+        for i in range(2, int(num1/2)):
+            if (num1 % i) == 0:
+                print("{} is not a prime number".format(num1))
+                break
+        else:
+            print("{} is a prime number".format(num1))
+
+
