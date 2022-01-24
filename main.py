554851e47af5c5a139e624b88ba9f878837fe5df
Kolin <colin.howard415@icloud.com>
2022-01-24 14:33:05 -0800

    Database Async and reformatting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
-from crud import app_crud
+from crud.app_crud import app_crud
+from crud.app_crud_api import app_crud_api
 from __init__ import app
 from flask import Blueprint
 import requests
@@ -11,6 +12,7 @@ from blueprints.colin import bcolin
 from blueprints.chase import bchase
 from blueprints.tanay import btanay
 # create a Flask instance
+app.register_blueprint(app_crud_api)
 app.register_blueprint(app_crud)
 app.register_blueprint(bconnor, url_prefix="/")
 app.register_blueprint(bpranav, url_prefix="/")
