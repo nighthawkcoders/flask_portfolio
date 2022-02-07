c365a3ce646978d45e8463a505107cc9bf3938e5
Yolanda <yolanda.sh2004@gmail.com>
2022-02-07 00:48:18 -0800

    currency

--- main.py
+++ main.py
@@ -357,6 +357,10 @@ def countries():
 def faq():
     return render_template("faq.html")
 
+@app.route('/currency', methods=['GET', 'POST'])
+def currency():
+    return render_template("layouts/currency.html")
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
