263deb3d3d44c2e11d58148896ad3c16e7901a79
John Mortensen <jmortensen@powayusd.com>
2022-07-21 21:12:57 -0700

    move project to blueprint directory

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
-from __init__ import app  # import initialization from "this" project
-from api import app_api # import api definition from "this" project
-app.register_blueprint(app_api) # register api routes
+# import "packages" from "this" project
+from __init__ import app  # Definitions initialization
+from api import app_api # Blueprint import api definition
+from bp_projects.projects import app_projects # Blueprint directory import projects definition
 
-# connects default URL to render index.html
-@app.route('/')
+app.register_blueprint(app_api) # register api routes
+app.register_blueprint(app_projects) # register api routes
+
+@app.errorhandler(404)  # catch for URL not found
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.route('/')  # connects default URL to index() function
 def index():
     return render_template("index.html")
 
-# connects /kangaroos path to render kangaroos.html
-@app.route('/portfolio/')
-def portfolio():
-    return render_template("projects/portfolio.html")
-
-# connects /kangaroos path to render kangaroos.html
-@app.route('/kangaroos/')
-def kangaroos():
-    return render_template("projects/kangaroos.html")
-
-@app.route('/walruses/')
-def walruses():
-    return render_template("projects/walruses.html")
-
-@app.route('/hawkers/')
-def hawkers():
-    return render_template("projects/hawkers.html")
-
-@app.route('/stub/')
+@app.route('/stub/')  # connects /stub/ URL to stub() function
 def stub():
     return render_template("stub.html")
 
-
-# runs the application on the development server
+# this runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
