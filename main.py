b97b1174c4ae51f6423ec3fa7a5f9a7203ccb152
John Mortensen <jmortensen@powayusd.com>
2023-02-03 16:39:51 -0800

    cors to support localhost

--- main.py
+++ main.py
@@ -43,5 +43,7 @@ def activate_job():
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
+    from flask_cors import CORS
+    cors = CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
     app.run(debug=True, host="0.0.0.0", port="8086")
