46b1464ccda8ec4a2a6f49b08d984a21524335dc
John Mortensen <jmortensen@powayusd.com>
2023-02-10 06:15:48 -0800

    SQLAlchemy 3.x required

--- main.py
+++ main.py
@@ -45,5 +45,4 @@ if __name__ == "__main__":
     # change name for testing
     from flask_cors import CORS
     cors = CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
     app.run(debug=True, host="0.0.0.0", port="8086")
