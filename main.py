a5fd4af2cfae27ffebedffd42dbdd8fccd9dd91b
John Mortensen <jmortensen@powayusd.com>
2023-02-21 21:42:49 -0800

    add student table

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
+from model.players import initPlayers
+
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
+from api.player import player_api
+
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -20,6 +24,7 @@ from projects.projects import app_projects # Blueprint directory import projects
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
+app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -40,6 +45,7 @@ def activate_job():  # activate these items
     db.init_app(app)
     initJokes()
     initUsers()
+    initPlayers()
 
 # this runs the application on the development server
 if __name__ == "__main__":
