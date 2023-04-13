39183f8aaf1931806fafb9dc06e74eb39f68a42e
John Mortensen <jmortensen@powayusd.com>
2023-04-12 17:41:55 -0700

    consolidate to single CRUD class

--- api/user.py
+++ api/user.py
@@ -12,8 +12,8 @@ user_api = Blueprint('user_api', __name__,
 api = Api(user_api)
 
 class UserAPI:        
-    class _Create(Resource):
-        def post(self):
+    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
+        def post(self): # Create method
             ''' Read data for json body '''
             body = request.get_json()
             
@@ -54,8 +54,7 @@ class UserAPI:
             # failure returns error
             return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
 
-    class _Read(Resource):
-        def get(self):
+        def get(self): # Read Method
             users = User.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
@@ -83,7 +82,6 @@ class UserAPI:
             
 
     # building RESTapi endpoint
-    api.add_resource(_Create, '/create')
-    api.add_resource(_Read, '/')
+    api.add_resource(_CRUD, '/')
     api.add_resource(_Security, '/authenticate')
     
