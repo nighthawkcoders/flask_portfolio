2e74ee669028a96cb94b901cf33930d1c18192c7
John Mortensen <jmortensen@powayusd.com>
2023-04-12 17:51:34 -0700

    change default names

--- model/users.py
+++ model/users.py
@@ -204,12 +204,11 @@ def initUsers():
         db.create_all()
         """Tester data for table"""
         u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko')
-        u3 = User(name='Alexander Graham Bell', uid='lex', password='123lex')
-        u4 = User(name='Eli Whitney', uid='whit', password='123whit')
-        u5 = User(name='John Mortensen', uid='jm1021', dob=date(1959, 10, 21))
+        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
+        u3 = User(name='Alexander Graham Bell', uid='lex')
+        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
 
-        users = [u1, u2, u3, u4, u5]
+        users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
         for user in users:
