66105c0091b25b2c55dd0fa206029a207d2a6a3e
aidanywu <aidanywu@gmail.com>
2023-11-23 23:57:09 -0800

    /authenticate returns generated JWT token User CRUD get and post now requires authentication

--- api/user.py
+++ api/user.py
@@ -1,7 +1,8 @@
-import json
-from flask import Blueprint, request, jsonify
+import json, jwt
+from flask import Blueprint, request, jsonify, current_app, Response
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
+from auth_middleware import token_required
 
 from model.users import User
 
@@ -13,7 +14,8 @@ api = Api(user_api)
 
 class UserAPI:        
     class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-        def post(self): # Create method
+        @token_required
+        def post(self, current_user): # Create method
             ''' Read data for json body '''
             body = request.get_json()
             
@@ -54,33 +56,67 @@ class UserAPI:
             # failure returns error
             return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 400
 
-        def get(self): # Read Method
+        @token_required
+        def get(self, current_user): # Read Method
             users = User.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
     
     class _Security(Resource):
-
         def post(self):
-            ''' Read data for json body '''
-            body = request.get_json()
-            
-            ''' Get Data '''
-            uid = body.get('uid')
-            if uid is None or len(uid) < 2:
-                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
-            password = body.get('password')
-            
-            ''' Find user '''
-            user = User.query.filter_by(_uid=uid).first()
-            if user is None or not user.is_password(password):
-                return {'message': f"Invalid user id or password"}, 400
-            
-            ''' authenticated user '''
-            return jsonify(user.read())
+            try:
+                body = request.get_json()
+                if not body:
+                    return {
+                        "message": "Please provide user details",
+                        "data": None,
+                        "error": "Bad request"
+                    }, 400
+                ''' Get Data '''
+                uid = body.get('uid')
+                if uid is None:
+                    return {'message': f'User ID is missing'}, 400
+                password = body.get('password')
+                
+                ''' Find user '''
+                user = User.query.filter_by(_uid=uid).first()
+                print(user._uid,user._password)
+                if user is None or not user.is_password(password):
+                    return {'message': f"Invalid user id or password"}, 400
+                if user:
+                    try:
+                        token = jwt.encode(
+                            {"_uid": user._uid},
+                            current_app.config["SECRET_KEY"],
+                            algorithm="HS256"
+                        )
+                        resp = Response("Authentication for %s successful" % (user._uid))
+                        resp.set_cookie("jwt", token,
+                                max_age=3600,
+                                secure=True,
+                                httponly=True,
+                                path='/'
+                                # domain="frontend.com"
+                                )
+                        return resp
+                    except Exception as e:
+                        return {
+                            "error": "Something went wrong",
+                            "message": str(e)
+                        }, 500
+                return {
+                    "message": "Error fetching auth token!, invalid email or password",
+                    "data": None,
+                    "error": "Unauthorized"
+                },
+            except Exception as e:
+                return {
+                        "message": "Something went wrong!",
+                        "error": str(e),
+                        "data": None
+                }, 500
 
             
-
     # building RESTapi endpoint
     api.add_resource(_CRUD, '/')
     api.add_resource(_Security, '/authenticate')
