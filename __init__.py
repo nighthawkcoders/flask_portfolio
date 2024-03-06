df4b8486aaec580507ec015ed666c9a88c10ec61
jm1021 <jmortensen@powayusd.com>
2024-03-05 16:04:33 -0800

    better way of handling orgins

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ These object can be used throughout project.
 
 # Setup of key Flask object (app)
 app = Flask(__name__)
-cors = CORS(app, supports_credentials=True)
+cors = CORS(app, supports_credentials=True, origins=['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io'])
 
 # Setup SQLAlchemy object and properties for the database (db)
 dbURI = 'sqlite:///volumes/sqlite.db'
--- main.py
+++ main.py
@@ -45,13 +45,6 @@ def index():
 def table():
     return render_template("table.html")
 
-@app.before_request
-def before_request():
-    # Check if the request came from a specific origin
-    allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
-        cors._origins = allowed_origin
-
 # Create an AppGroup for custom commands
 custom_cli = AppGroup('custom', help='Custom commands')
 
